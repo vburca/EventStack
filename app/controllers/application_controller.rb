1b6d6d684e19468ab0822f0b7878cf6e75b8666b
Vlad Burca <vladburca2005@gmail.com>
2013-03-16 03:33:45 -0400

    Changes on scaffold stylesheets. Finally working.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   protected
+    # Tweak this a bit so that it logs if they are forcing one of the admin pages.
+    # Also, might want to throw a flash message? Saying that it is only for admins? Or maybe not...
+    
     def authenticate_admin!
       if !current_user.admin?
         redirect_to home_path
